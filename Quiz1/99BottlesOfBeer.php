0e0ed79d242569a6f91b9840c2e43b3d05985931
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-12-06 19:28:04 -0500

    first commit

---
+++ Quiz1/99BottlesOfBeer.php
@@ -0,0 +1,33 @@
+<?php
+
+$title = '99 Bottles of Beer<hr>';
+function song() {
+    for ($bottles = 99; $bottles > 0; $bottles--) {
+        if ($bottles > 1) {
+            echo $bottles . " bottles of beer on the wall, " . $bottles . " bottles of beer. ";
+            echo "Take one down and pass it around, " . ($bottles - 1) . " bottles of beer on the wall.</br>";
+            echo '<br/>';
+        } else if ($bottles == 1) {
+            echo $bottles . " bottle of beer on the wall, " . $bottles . " bottle of beer. Take it down and pass it around, no more bottles of beer on the wall.</br>";
+            echo '<br/>';
+        }
+
+    }
+    echo "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>99 Bottles of Beer</title>
+</head>
+<body>
+    <h1><pre><?php echo $title; ?></pre></h1>
+    <p><pre><?php song(); ?></pre></p>
+</body>
+</html>
