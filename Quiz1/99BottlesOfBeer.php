c174e59f3697480739fc5a5ed68f4d6bd5e6d3c7
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-12-08 00:10:31 -0500

    Updated

--- Quiz1/99BottlesOfBeer.php
+++ Quiz1/99BottlesOfBeer.php
@@ -1,24 +1,30 @@
 <?php
 $title = '99 Bottles of Beer<hr>';
 function song() {
-    for ($bottles = 99; $bottles > 0; $bottles--) {
-        if ($bottles > 2) {
+    echo "<font size='4'>"; 
+    for ($bottles = 99; $bottles > 0; $bottles--) {   
+        echo "<font color='#8A2BE2'>";   
+        if ($bottles > 2) { 
+            if(($bottles % 2) === 0){
+                echo "<font color='#FF00FF'>";
+            } 
             echo $bottles . " bottles of beer on the wall, " . $bottles . " bottles of beer. ";
             echo "Take one down and pass it around, " . ($bottles - 1) . " bottles of beer on the wall.</br>";
             echo '<br/>';
         } else if ($bottles > 1) {
+            echo "<font color='#FF00FF'>";
             echo $bottles . " bottles of beer on the wall, " . $bottles . " bottles of beer. ";
-            echo "Take one down and pass it around, " . ($bottles - 1) . " bottle of beer on the wall.</br>";
+            echo "Take one down and pass it around, " . ($bottles - 1) . " bottle of beer on the wall.</br>";  
             echo '<br/>';
-        } else if ($bottles == 1) {
+        } else if ($bottles === 1) {  
             echo $bottles . " bottle of beer on the wall, " . $bottles . " bottle of beer. Take it down and pass it around, no more bottles of beer on the wall.</br>";
             echo '<br/>';
         }
-    }
-    echo "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";
+    } 
+    echo "<font color='#FF00FF'>"; 
+    echo "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.";   
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,6 +35,6 @@ function song() {
 </head>
 <body>
     <h1><pre><?php echo $title; ?></pre></h1>
-    <p><pre><?php song(); ?></pre></p>
+    <p><strong><pre><?php song(); ?></pre></strong></p>
 </body>
 </html>
